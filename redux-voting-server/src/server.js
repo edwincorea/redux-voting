ad393db3083c15f872a3b4a1fcc6d268184faca3
Edwin Corea <Edwin Corea>
2017-03-11 15:11:58 -0600

    Receiving Remote Redux Actions.

--- redux-voting-server/src/server.js
+++ redux-voting-server/src/server.js
@@ -15,6 +15,16 @@ export default function startServer(store) {
   //In the event listener we can emit the current state right away.
   io.on('connection', (socket) => {
     socket.emit('state', store.getState().toJS());
+    //have our clients emit 'action' events that we feed directly into our Redux store
+    socket.on('action', store.dispatch.bind(store));
   });
+
+  // A client sends an action to the server.
+  // The server hands the action to the Redux Store.
+  // The Store calls the reducer and the reducer executes the logic related to the action.
+  // The Store updates its state based on the return value of the reducer.
+  // The Store executes the listener function subscribed by the server.
+  // The server emits a 'state' event.
+  // All connected clients - including the one that initiated the original action - receive the new state.  
 
 }
