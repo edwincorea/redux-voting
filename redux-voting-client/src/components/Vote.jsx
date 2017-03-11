b6428846d7b91c342b0b4b9a6d5d9b866062508e
Edwin Corea <Edwin Corea>
2017-03-11 17:44:01 -0600

    Writing The UI for The Voting Screen.

---
+++ redux-voting-client/src/components/Vote.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+export default class Vote extends Component {    
+  constructor(props){
+      super(props);      
+
+      this._getPair = this._getPair.bind(this);
+      this._isDisabled = this._isDisabled.bind(this);
+      this._hasVotedFor = this._hasVotedFor.bind(this);
+  }
+
+  _getPair() {
+    return this.props.pair || [];
+  }
+
+  _isDisabled() {
+    return !!this.props.hasVoted;
+  }
+
+  _hasVotedFor(entry) {
+    return this.props.hasVoted === entry;
+  }    
+
+  render() {
+    return <div className="voting">
+      {this._getPair().map(entry =>
+        <button key={entry}
+                disabled={this._isDisabled()}
+                onClick={() => this.props.vote(entry)}>
+          <h1>{entry}</h1>
+          {this._hasVotedFor(entry) ?
+            <div className="label">Voted</div> :
+            null}
+        </button>
+      )}
+    </div>;      
+  }
+}
