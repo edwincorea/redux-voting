fab4ec246ba3606c448f20efe63946d141770a44
Edwin Corea <Edwin Corea>
2017-03-11 22:02:59 -0600

    Getting Data In from Redux to React.

--- redux-voting-client/test/components/Voting_spec.jsx
+++ redux-voting-client/test/components/Voting_spec.jsx
@@ -7,7 +7,7 @@ import { renderIntoDocument,
 import {List} from 'immutable';
 import {expect} from 'chai';
 
-import Voting from '../../src/components/Voting';
+import {Voting} from '../../src/components/Voting';
 
 describe('Voting', () => {
 
